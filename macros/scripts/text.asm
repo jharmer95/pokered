b71f62f8b039a5212cd76eced3d0163858e8b8f2
Jackson Harmer <jharmer95@gmail.com>
2020-08-19 19:44:33 -0400

    Fixed Nidorino cry issue
    Previously, during Professor Oak's introduction, while a Nidorino was shown on screen, a Nidorina's cry was heard.
    Now, the proper Nidorino cry is heard instead.

--- macros/scripts/text.asm
+++ macros/scripts/text.asm
@@ -127,9 +127,9 @@ sound_dex_page_added: MACRO
 	db TX_SOUND_DEX_PAGE_ADDED
 ENDM
 
-	const TX_SOUND_CRY_NIDORINA ; $14
-sound_cry_nidorina: MACRO
-	db TX_SOUND_CRY_NIDORINA
+	const TX_SOUND_CRY_NIDORINO ; $14
+sound_cry_nidorino: MACRO
+	db TX_SOUND_CRY_NIDORINO
 ENDM
 
 	const TX_SOUND_CRY_PIDGEOT ; $15
